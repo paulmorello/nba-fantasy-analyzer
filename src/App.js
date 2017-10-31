11c07d513d62ed2efca7bfdffc0a2264949059e0
paulmorello <paulmorello1@gmail.com>
2017-10-30 20:44:07 -0700

    added Team component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// File imports
+import Team from './Team';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>NBA Trade Analyzer</h1>
+        <Team />
+        <Team />
       </div>
     );
   }
---
+++ src/Team.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Team = () =>
+
+  <div className="team-container">
+    <form>
+      <input type="text" value="" placeholder="Enter a player" />
+      <button type="submit" name="submit" value="submit">Submit</button>
+    </form>
+  </div>;
+
+
+export default Team;
