39ec1088bfa2ddb27efca06e4337cd193d4e3886
paulmorello <paulmorello1@gmail.com>
2017-10-31 21:20:47 -0700

    Navbar component created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 // File imports
 import Team from './Team';
 
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +12,7 @@ class App extends Component {
         <h1>NBA Trade Analyzer</h1>
         <Team />
         <Team />
+
       </div>
     );
   }
---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () =>
+
+  <header>
+    <nav>
+      <div>
+        <h3>Trade Analyzer</h3>
+      </div>
+      <div>
+        <ul>
+          <li>Link 1</li>
+          <li>Link 2</li>
+          <li>Link 3</li>
+        </ul>
+      </div>
+    </nav>
+  </header>;
+
+
+export default Navbar;
