9c803be7d417f4f7aa6a3d4f7dc491cff23d02b8
paulmorello <paulmorello1@gmail.com>
2017-11-20 14:58:19 -0500

    updated score team components

--- src/components/ScoreGetTeam.js
+++ src/components/ScoreGetTeam.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 const ScoreGetTeam = props =>
 
   <div className="get-team-score-container">
-    <div><strong>Team Score: </strong>{ props.getPlayerScore }</div>
-    <div><strong>Trade Diff: </strong>{ props.getPlayerScore - props.givePlayerScore }</div>
+    <div>Team Score: { props.getPlayerScore }</div>
+    <div>Trade Diff: { props.getPlayerScore - props.givePlayerScore }</div>
   </div>;
 
   ScoreGetTeam.propTypes = {
