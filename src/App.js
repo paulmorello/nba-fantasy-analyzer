a39127ac1ae9cbe3c419f50ea87761efb71bda58
paulmorello <paulmorello1@gmail.com>
2017-11-29 15:38:11 -0500

    added more advanced stats to each player

--- src/App.js
+++ src/App.js
@@ -27,18 +27,44 @@ class App extends Component {
                               .then( data => {
                                 return data;
                               })
-    const newPlayerStats = await NBA.stats.playerProfile({ 
+    const newPlayerStats = await NBA.stats.playerProfile({
                                   PlayerID: player.playerId,
-                                  Season: "2016-17"
+                                  Season: "2017-18"
                                 })
                                 .then( data => {
                                   console.log(data);
+                                  return data;
                                 })
+
+    const playerCurrentSeasonStats = newPlayerStats.seasonTotalsRegularSeason
+                                  .filter( season => season.seasonId === "2017-18" );
 
     const addPlayerToState = {
       name: playerStats.playerHeadlineStats[0].playerName,
+      season: playerCurrentSeasonStats[0].seasonId,
+      team: playerCurrentSeasonStats[0].teamAbbreviation,
       points: playerStats.playerHeadlineStats[0].pts,
       assists: playerStats.playerHeadlineStats[0].ast,
+      rebounds: playerCurrentSeasonStats[0].reb,
+      orebounds: playerCurrentSeasonStats[0].oreb,
+      drebounds: playerCurrentSeasonStats[0].dreb,
+      blocks: playerCurrentSeasonStats[0].blk,
+      steals: playerCurrentSeasonStats[0].stl,
+      fg3a: playerCurrentSeasonStats[0].fG3A,
+      fg3m: playerCurrentSeasonStats[0].fG3M,
+      fg3Pct: playerCurrentSeasonStats[0].fg3Pct,
+      fgPct: playerCurrentSeasonStats[0].fgPct,
+      fga: playerCurrentSeasonStats[0].fga,
+      fgm: playerCurrentSeasonStats[0].fgm,
+      ftPct: playerCurrentSeasonStats[0].ftPct,
+      fta: playerCurrentSeasonStats[0].fta,
+      ftm: playerCurrentSeasonStats[0].ftm,
+      gp: playerCurrentSeasonStats[0].gp,
+      gs: playerCurrentSeasonStats[0].gs,
+      min: playerCurrentSeasonStats[0].min,
+      pf: playerCurrentSeasonStats[0].pf,
+      tov: playerCurrentSeasonStats[0].tov,
+      age: playerCurrentSeasonStats[0].playerAge,
       rating: 99,
       toGive: false,
       toGet: false
