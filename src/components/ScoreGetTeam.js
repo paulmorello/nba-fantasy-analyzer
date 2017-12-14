8af0722213f3a9981d1b1911bbe5fe4f7434e05f
paulmorello <paulmorello1@gmail.com>
2017-12-13 21:29:18 -0500

    updated styles

--- src/components/ScoreGetTeam.js
+++ src/components/ScoreGetTeam.js
@@ -9,14 +9,18 @@ const ScoreGetTeam = props =>
       <span className="team-score">Trade Diff: </span> { props.getPlayerScore - props.givePlayerScore }
     </div>
     <div>
-      <span>FG%: { ((props.playerStatDiff[0][4] - props.playerStatDiff[1][4]) * 100 ).toFixed(2) }</span>,
-      <span> FT%: { ((props.playerStatDiff[0][5] - props.playerStatDiff[1][5]) * 100 ).toFixed(2) }</span>,
-      <span> Pts: { (props.playerStatDiff[0][0] - props.playerStatDiff[1][0]).toFixed(2) }</span>,
-      <span> Reb: { (props.playerStatDiff[0][1] - props.playerStatDiff[1][1]).toFixed(2) }</span>,
-      <span> Ast: { (props.playerStatDiff[0][2] - props.playerStatDiff[1][2]).toFixed(2) }</span>,
-      <span> 3PM: { (props.playerStatDiff[0][3] - props.playerStatDiff[1][3]).toFixed(2) }</span>,
-      <span> Blk: { (props.playerStatDiff[0][6] - props.playerStatDiff[1][6]).toFixed(2) }</span>,
-      <span> Stl: { (props.playerStatDiff[0][7] - props.playerStatDiff[1][7]).toFixed(2) }</span>
+      <div>
+        <span>FG%: { ((props.playerStatDiff[0][4] - props.playerStatDiff[1][4]) * 100 ).toFixed(2) }</span>,
+        <span> FT%: { ((props.playerStatDiff[0][5] - props.playerStatDiff[1][5]) * 100 ).toFixed(2) }</span>,
+        <span> Pts: { (props.playerStatDiff[0][0] - props.playerStatDiff[1][0]).toFixed(2) }</span>,
+        <span> Reb: { (props.playerStatDiff[0][1] - props.playerStatDiff[1][1]).toFixed(2) }</span>
+      </div>
+      <div>
+        <span> Ast: { (props.playerStatDiff[0][2] - props.playerStatDiff[1][2]).toFixed(2) }</span>,
+        <span> 3PM: { (props.playerStatDiff[0][3] - props.playerStatDiff[1][3]).toFixed(2) }</span>,
+        <span> Blk: { (props.playerStatDiff[0][6] - props.playerStatDiff[1][6]).toFixed(2) }</span>,
+        <span> Stl: { (props.playerStatDiff[0][7] - props.playerStatDiff[1][7]).toFixed(2) }</span>
+      </div>
     </div>
   </div>;
 
