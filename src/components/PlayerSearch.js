95b48446856e757f042bbc76fcec723c22ae4bc3
paulmorello <paulmorello1@gmail.com>
2018-01-11 20:55:30 -0500

    aded input to PlayerSearch component

--- src/components/PlayerSearch.js
+++ src/components/PlayerSearch.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+// File imports
+import Autocomplete from './Autocomplete.js';
 
 class PlayerSearch extends Component {
 
   render() {
     return (
       <div className="player-search-container">
+        <input
+          type="text"
+          placeholder="Enter a player" />
+          <Autocomplete />
+        <button>Add Player</button>
       </div>
     )
   }
