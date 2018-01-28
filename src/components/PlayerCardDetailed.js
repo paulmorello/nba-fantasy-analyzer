13654ef34f6e3f9eccba0116e563fa5ef111de16
paulmorello <paulmorello1@gmail.com>
2018-01-28 10:34:52 +0000

    added PlayerStatsCard to PlayerCardDetailed

--- src/components/PlayerCardDetailed.js
+++ src/components/PlayerCardDetailed.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+// File imports
+import PlayerStatsCard from './PlayerStatsCard';
+
 class PlayerCardDetailed extends Component {
 
   render() {
     return (
       <div className="player-card-detailed-container">
-        
+        <PlayerStatsCard />
       </div>
     )
   }
