c3e607737f6b9d025a97ef2a4bfcee749ebe2b5c
paulmorello <paulmorello1@gmail.com>
2018-01-28 10:30:50 +0000

    created player stats card

--- src/components/PlayerCardDetailed.js
+++ src/components/PlayerCardDetailed.js
@@ -6,6 +6,7 @@ class PlayerCardDetailed extends Component {
   render() {
     return (
       <div className="player-card-detailed-container">
+        
       </div>
     )
   }
---
+++ src/components/PlayerStatsCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlayerStatsCard extends Component {
+
+  render() {
+    return (
+      <div className="player-stats-card">
+      </div>
+    )
+  }
+}
+
+  PlayerStatsCard.propTypes = {
+  }
+
+
+export default PlayerStatsCard;
