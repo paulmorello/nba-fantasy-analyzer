0920a2c3cafae7a2dc8ec38c10bcc69476430880
paulmorello <paulmorello1@gmail.com>
2018-12-30 22:10:19 +1100

    Added more details to the player stats card

--- src/components/PlayerCardDetailed.js
+++ src/components/PlayerCardDetailed.js
@@ -38,6 +38,25 @@ class PlayerCardDetailed extends Component {
             name={player.name}
             age={player.age}
             team={player.team}
+            gs={player.gs}
+            gp={player.gp}
+            min={player.min}
+            points={player.points}
+            assists={player.assists}
+            blocks={player.blocks}
+            rebounds={player.rebounds}
+            steals={player.steals}
+            fp3Pct={player.fp3Pct}
+            fg3m={player.fg3m}
+            fg3a={player.fg3a}
+            fgPct={player.fgPct}
+            fga={player.fga}
+            fgm={player.fgm}
+            ftPct={player.ftPct}
+            fta={player.fta}
+            ftm={player.ftm}
+            pf={player.pf}
+            tov={player.tov}
           />
         )}
       </div>
--- src/components/PlayerStatsCard.js
+++ src/components/PlayerStatsCard.js
@@ -13,7 +13,25 @@ class PlayerStatsCard extends Component {
             { this.props.name }, { this.props.age }, { this.props.team }
           </div>
           <div className="player-profile-stats">
-
+            { this.props.gs },
+            { this.props.gp },
+            { this.props.min },
+            { this.props.points },
+            { this.props.assists },
+            { this.props.blocks },
+            { this.props.rebounds },
+            { this.props.steals },
+            { this.props.fg3Pct },
+            { this.props.fg3m },
+            { this.props.fg3a },
+            { this.props.fgPct },
+            { this.props.fga },
+            { this.props.fgm },
+            { this.props.ftPct },
+            { this.props.fta },
+            { this.props.ftm },
+            { this.props.pf },
+            { this.props.tov }
           </div>
         </div>
       </div>
@@ -24,7 +42,26 @@ class PlayerStatsCard extends Component {
   PlayerStatsCard.propTypes = {
     name: PropTypes.string,
     age: PropTypes.number,
-    team: PropTypes.string
+    team: PropTypes.string,
+    gs: PropTypes.number,
+    gp: PropTypes.number,
+    min: PropTypes.number,
+    points: PropTypes.number,
+    assists: PropTypes.number,
+    blocks: PropTypes.number,
+    rebounds: PropTypes.number,
+    steals: PropTypes.number,
+    fp3Pct: PropTypes.number,
+    fg3m: PropTypes.number,
+    fg3a: PropTypes.number,
+    fgPct: PropTypes.number,
+    fga: PropTypes.number,
+    fgm: PropTypes.number,
+    ftPct: PropTypes.number,
+    fta: PropTypes.number,
+    ftm: PropTypes.number,
+    pf: PropTypes.number,
+    tov: PropTypes.number  
   }
 
 
